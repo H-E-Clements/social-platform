28bc33c9c12916e215ce5d187e62ce75ba392820
marwanosman505 <marwanosman505@gmail.com>
2023-03-26 17:55:29 +0100

    added math functionality

--- simulator/script.js
+++ simulator/script.js
@@ -1,6 +1,85 @@
 let numLines = 1;
 let currentLine = 0;
 
+// characters used to identify variable in expression
+const regex = /[a-zA-Z%]/;
+
+const expression = '5+    9';
+const result = evaluateExpression(expression);
+console.log(`The result of ${expression} is ${result}`);
+
+// function evaluates mathematical expression from string
+function stringMath(eq, callback) {
+  if (typeof eq !== 'string') return handleCallback(new TypeError('The [String] argument is expected.'), null);
+  const mulDiv = /([+-]?\d*\.?\d+(?:e[+-]\d+)?)\s*([*/])\s*([+-]?\d*\.?\d+(?:e[+-]\d+)?)/;
+  const plusMin = /([+-]?\d*\.?\d+(?:e[+-]\d+)?)\s*([+-])\s*([+-]?\d*\.?\d+(?:e[+-]\d+)?)/;
+  const parentheses = /(\d)?\s*\(([^()]*)\)\s*/;
+  var current;
+  while (eq.search(/^\s*([+-]?\d*\.?\d+(?:e[+-]\d+)?)\s*$/) === -1) {
+    eq = fParentheses(eq);
+    if (eq === current) return handleCallback(new SyntaxError('The equation is invalid.'), null);
+    current = eq;
+  }
+  return handleCallback(null, +eq);
+
+  function fParentheses(eq) {
+    while (eq.search(parentheses) !== -1) {
+      eq = eq.replace(parentheses, function (a, b, c) {
+        c = fMulDiv(c);
+        c = fPlusMin(c);
+        return typeof b === 'string' ? b + '*' + c : c;
+      });
+    }
+    eq = fMulDiv(eq);
+    eq = fPlusMin(eq);
+    return eq;
+  }
+
+  function fMulDiv(eq) {
+    while (eq.search(mulDiv) !== -1) {
+      eq = eq.replace(mulDiv, function (a) {
+        const sides = mulDiv.exec(a);
+        const result = sides[2] === '*' ? sides[1] * sides[3] : sides[1] / sides[3];
+        return result >= 0 ? '+' + result : result;
+      });
+    }
+    return eq;
+  }
+
+  function fPlusMin(eq) {
+    eq = eq.replace(/([+-])([+-])(\d|\.)/g, function (a, b, c, d) { return (b === c ? '+' : '-') + d; });
+    while (eq.search(plusMin) !== -1) {
+      eq = eq.replace(plusMin, function (a) {
+        const sides = plusMin.exec(a);
+        return sides[2] === '+' ? +sides[1] + +sides[3] : sides[1] - sides[3];
+      });
+    }
+    return eq;
+  }
+
+  function handleCallback(errObject, result) {
+    if (typeof callback !== 'function') {
+      if (errObject !== null) throw errObject;
+    } else {
+      callback(errObject, result);
+    }
+    return result;
+
+  }
+
+}
+
+// helper function for stringMath()
+function evaluateExpression(expression) {
+  try {
+    return stringMath(expression);
+  } catch (error) {
+    console.log(`Error evaluating expression: ${error.message}`);
+    return null;
+  }
+}
+
+
 optionValues = [{name: "x", value: 0, stack: []}, {name: "y", value: 1, stack: []}, {name: "z", value: 0, stack: []}]
 
 const addLine = () => {
@@ -104,8 +183,19 @@ const addLine = () => {
         break;
       }
     };
-    optionValues[varIndex].stack.push(optionValues[varIndex].value);
-    optionValues[varIndex].value = lineText;
+    if (regex.test(lineText)) {
+      let newExpression = lineText; 
+      // replace variable name with variable value within expression
+      optionValues.forEach(element => {
+        newExpression = newExpression.replace('%'+element.name, element.value)
+      });
+      console.log(newExpression);
+      optionValues[varIndex].stack.push(evaluateExpression(newExpression));
+      optionValues[varIndex].value = evaluateExpression(newExpression);
+    } else {
+      optionValues[varIndex].stack.push(optionValues[varIndex].value);
+      optionValues[varIndex].value = lineText;
+    }
     updateCurrentState();
   }
 
@@ -115,9 +205,11 @@ const addLine = () => {
         executeLine();
         document.getElementById("line"+currentLine+"Text").disabled = true;
         document.getElementById("line"+currentLine+"Select").disabled = true;
+        if (currentLine < numLines) {
+          lineDiv = document.getElementById("line"+currentLine);
+          lineDiv.classList.add("highlighted");
+        }
         currentLine += 1;
-        lineDiv = document.getElementById("line"+currentLine);
-        lineDiv.classList.add("highlighted");
       } catch (e) {
         console.error(e);
       }
